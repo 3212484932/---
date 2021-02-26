ac37ae9e006e26864c2cf26744b7d0edd433a950
An <3212484932@qq.com>
2021-02-26 23:40:42 +0800

    完成了购物车页面的静态渲染

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,66 +1,82 @@
-// pages/cart/index.js
+/**
+ * 1. 调用小程序的内置 API 获取用户的地址 wx.chooseAddress
+ * 2. 获取用户 对小程序 所授权 获取地址的 权限状态 scope
+ *    1. 假设 用户获取的收货地址的提示框 确定 authSetting scope.address
+ *       scope 值 true 直接调用 获取收货地址
+ *    2. 假设 用户从来没有调用过 收货地址的 API
+ *       scope为 undefined 直接调用 获取收货地址
+ *    3. 假设用户 点击获取收货地址的提示框 取消
+ *       scope 值 false
+ *       1. 诱导用户自己打开授权设置页面 当用户重新给予 获取地址权限的时候
+ *       2. 获取收货地址
+ * 3. 页面加载完毕
+ *    0 onLoad onShow
+ *    1 获取本地存储中的地址数据
+ *    2 把数据 设置为 data 中的一个变量
+ *
+ */
+
+import { getSetting, chooseAddress, openSetting } from "../../utils/asyncWx.js";
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    address: {},
+  },
+  onShow() {
+    // 1. 获取缓存中的收货地址信息
+    const address = wx.getStorageSync("address");
+    // 2. 给data 赋值
+    this.setData({
+      address,
+    });
   },
 
   /**
-   * 生命周期函数--监听页面加载
+   * 获取地址的处理事件函数
    */
-  onLoad: function (options) {
+  async handleChooseAddress() {
+    // console.log("111");
+    // 调用小程序默认的插件 获取地址
+    // 1 获取 权限状态
+    try {
+      const res1 = await getSetting();
+      const scopeAddress = res1.authSetting["scope.address"];
+      // 2 判断 权限状态
+      if (scopeAddress === false) {
+        await openSetting();
+      }
+      // 4 调用获取收货地址的 api
+      let address = await chooseAddress();
+      // console.log(address);
+      // 整理数据格式
+      address.all =
+        address.provinceName +
+        " " +
+        address.cityName +
+        " " +
+        address.countyName +
+        " " +
+        address.detailInfo;
+      // 5. 存入缓存中
 
+      wx.setStorageSync("address", address);
+    } catch (error) {
+      console.log(error);
+    }
+
+    // // 1. 获取权限状态
+    // const res1 = await getSetting();
+    // let scopeAddress = res1.authSetting["scope.address"];
+    // // 2. 判断权限状态
+    // if (scopeAddress === true || scopeAddress === undefined) {
+    //   // 3. 调用获取收货地址 api
+    //   const res2 = await chooseAddress();
+    //   console.log(res2);
+    // } else {
+    //   // 4. 诱导用户打开授权页面
+    //   await openSetting();
+    //   // 5. 调用获取收货地址的 API
+    //   const res2 = await chooseAddress();
+    //   console.log(res2);
+    // }
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+});
--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -3,6 +3,13 @@
  * 2. 点击轮播图 预览大图片
  *    1. 给轮播图绑定点击事件
  *    2. 调用小程序的 API previewImage
+ * 3. 点击加入购物车
+ *    1. 先绑定点击事件
+ *    2. 获取缓存中的购物车数据
+ *    3. 先判断当前商品是否存在购物车
+ *    4. 已经存在 修改商品数据 执行购物车数量++ 重新把购物车的数组 填充回缓存中
+ *    5. 不存在于购物车数组中，直接给购物车的数组添加一个新元素 带上购物车的属性 num 重新把购物车的数组 填充回缓存中
+ *    6. 弹窗提示
  */
 import { request } from "../../request/index.js";
 import regeneratorRuntime from "../../lib/runtime/runtime.js";
@@ -60,5 +67,34 @@ Page({
       current, // 当前显示图片的http链接
       urls, // 需要预览的图片http链接列表
     });
+  },
+  // 添加购物车事件处理函数
+  handleCartAdd() {
+    // console.log("购物车");
+    //  *    2. 获取缓存中的购物车数据
+    let cart = wx.getStorageSync("cart") || [];
+    //  *    3. 先判断当前商品是否存在购物车
+    let index = cart.findIndex((v) => v.goods_id === this.GoodsInfo.goods_id);
+    if (index === -1) {
+      // 不存在 第一次添加
+      this.GoodsInfo.num = 1;
+      this.GoodsInfo.checked = true;
+      cart.push(this.GoodsInfo);
+    } else {
+      // 存在 商品数量++
+      cart[index].num++;
+    }
+    // 把购物车重新添加回缓存中
+    wx.setStorageSync("cart", cart);
+    // 弹窗提示
+    wx.showToast({
+      title: "添加成功",
+      // true 防止用户手抖疯狂点击
+      mask: true,
+    });
+
+    //  *    4. 已经存在 修改商品数据 执行购物车数量++ 重新把购物车的数组 填充回缓存中
+    //  *    5. 不存在于购物车数组中，直接给购物车的数组添加一个新元素 带上购物车的属性 num 重新把购物车的数组 填充回缓存中
+    //       6. 弹窗提示
   },
 });
